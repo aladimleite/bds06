3a0efe4751efc9cd8a10166d66dfba53e7f0d144
Aladim Santana <aladimleite@gmail.com>
2024-02-24 22:41:10 -0300

    Preauthorizing methods

--- bds06/src/main/java/com/devsuperior/movieflix/resources/ReviewResource.java
+++ bds06/src/main/java/com/devsuperior/movieflix/resources/ReviewResource.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,8 @@ public class ReviewResource {
 	
 	@Autowired
 	private ReviewService service;
-	
+
+	@PreAuthorize("hasAnyRole('MEMBER')")
 	@PostMapping
 	public ResponseEntity<ReviewDTO> insert(@Valid @RequestBody ReviewDTO reviewUserDTO) {
 		
